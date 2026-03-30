5062275f0f7eff397d7c57340f7719b467536b9a
Daniel Costa <danielcosta@saude-go.net>
2026-03-30 18:16:19 -0300

    feat: adiciona DTOs e validador de entrada para assinatura

---
+++ modules/safira-rest/src/main/java/br/gov/go/saude/fhir/safira/rest/dto/SigningInputValidator.java
@@ -0,0 +1,36 @@
+package br.gov.go.saude.fhir.safira.rest.dto;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import java.time.Instant;
+
+@Component
+public class SigningInputValidator implements Validator {
+
+    private static final long MAX_TIME_DRIFT_SECONDS = 300; // ±5 minutes
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return SigningInput.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        SigningInput input = (SigningInput) target;
+
+        if (input.referenceTimestamp() != null) {
+            long currentServerTime = Instant.now().getEpochSecond();
+            long timeDiff = Math.abs(currentServerTime - input.referenceTimestamp());
+            
+            if (timeDiff > MAX_TIME_DRIFT_SECONDS) {
+                errors.rejectValue(
+                        "referenceTimestamp",
+                        "timestamp.outofsync",
+                        "O timestamp de referência fornecido excede a tolerância de ±5 minutos em relação ao relógio do servidor UTC."
+                );
+            }
+        }
+    }
+}
